f55de90ea7e111c20ee6258e78694e95d2bba3ac
Spatialflunky1 <mjandrus06@gmail.com>
2021-08-15 13:56:58 -0400

    added beginig chatbot html

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, request, url_for
-import argparse
 
 app = Flask(__name__)
 
@@ -11,14 +10,10 @@ def home():
 if __name__ == '__main__':
     ip = "192.168.68.111" 
     port = "8000"
-    ap = argparse.ArgumentParser()
     if "ip" not in locals():
         ip = input("Enter the IP: ")
     if "port" not in locals():
         port = int(input("Enter the Port: "))
-    ap.add_argument("-f", "--frame-count", type=int, default=32, help="number of frames used to construct the bacground model")
-    args = vars(ap.parse_args())
-    x = 0
     while True:
         debug = input("Debug Mode (changes to local only)? (y/n): ")
         if debug == "y" or debug == "n":
