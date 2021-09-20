fb8e2475dc1576e8ba0af75e0041198c5e81b3fb
Spatialflunky1 <mjandrus06@gmail.com>
2021-09-19 21:45:56 -0400

    added working chat

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 from flask import Flask, render_template, redirect, request, url_for
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    print(request.method)
     if request.method == "POST":
-        return redirect(url_for("irc"))
+        if request.form['button'] == "About":
+            return redirect(url_for("about"))
+        if request.form['button'] == "Enter Chat":
+            return redirect(url_for("chat"))
     return render_template("home.html")
 
-@app.route("/irc", methods=["GET", "POST"])
-def irc():
-    return render_template("irc.html")
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    return render_template("chat.html")
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+    send(data, broadcast=True)
+
 
 if __name__ == '__main__':
     ip = "localhost" 
-    port = "80"
+    port = 8000
     if "ip" not in locals():
         ip = input("Enter the IP: ")
     if "port" not in locals():
         port = int(input("Enter the Port: "))
-    while True:
-        debug = input("Debug Mode (changes to local only)? (y/n): ")
-        if debug == "y" or debug == "n":
-            break
-        else:
-            print("Invalid Response")
-    if debug == "y":
-        debug = True
-    elif debug == "n":
-        debug == False
-    app.run(host=ip, port=port, debug=debug, threaded=True, use_reloader=False)
+    socketio.run(app, host=ip, port=port)
